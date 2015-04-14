bb0111794f371b21f6705abd223ae7cc21aca4ea
StianHB <stian.hig@hig.no>
2015-04-14 20:56:06 +0200

    Powershell is magic

---
+++ fnamecheck.ps1
@@ -0,0 +1,3 @@
+﻿$folder = $args[0]
+
+Get-ChildItem $folder -Recurse | Where-Object {$_.Name -match "[æøå\s]"}
---
+++ fsinfo.ps1
@@ -0,0 +1,23 @@
+﻿$folder=Get-item $args[0]
+ 
+[string]$root=$($folder.Root)
+
+$root = $root.Replace("\","")
+
+$diskObject=$(gwmi win32_logicaldisk | Where-Object {$_.DeviceID -eq "$root"})
+
+[long]$freeSpace=$($diskObject | Format-Table FreeSpace -HideTableHeaders | Out-String)
+[long]$totalSpace=$($diskObject | Format-Table Size -HideTableHeaders | Out-String)
+
+$used= 100-(($freeSpace/$totalSpace)*100)
+
+$files=$(Get-ChildItem $folder -Recurse | Where-Object { ! $_.PSIsContainer } | sort -Descending length)
+$nr=$($files.count)
+$largest=$($files | Select-Object -First 1 | %{$_.FullName} | Out-String)
+$largestSize=$($files | Select-Object -first 1 | %{$_.Length} | Out-String | .\human-readable-bytes.ps1 )
+$average=$($files | Measure-Object -Average Length | Format-Table Average -HideTableHeaders | Out-String | %{$_.Replace(',',".")} | .\human-readable-bytes.ps1)
+
+Write-Output "Partisjonen $folder befinner seg på er $($used)% full."
+Write-Output  "Det finnes $nr filer."
+Write-Output "Den største er $largest som er $largestSize stor."
+Write-Output "Gjennomsnittlig filstørrelse er $average"
